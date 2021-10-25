014971ee35c8ed24917101484d61b33d813988dc
su <wkdlfflxh@naver.com>
2021-10-25 17:55:37 +0900

    Feat: 새 프로젝트 생성

---
+++ DuelDice/DuelDice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DuelDice
+//
+//  Created by su on 2021/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
