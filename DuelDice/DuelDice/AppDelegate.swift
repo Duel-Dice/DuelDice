2b6962d8de4bc62780d75251a52ebc92b623f07f
su <wkdlfflxh@naver.com>
2021-12-05 03:11:34 +0900

    Feat: facebook login

--- DuelDice/DuelDice/AppDelegate.swift
+++ DuelDice/DuelDice/AppDelegate.swift
@@ -8,15 +8,19 @@
 import UIKit
 import Firebase
 import GoogleSignIn
+import FBSDKCoreKit
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        ApplicationDelegate.shared.application(
+            application,
+            didFinishLaunchingWithOptions: launchOptions
+        )
         return true
     }
 
@@ -38,9 +42,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, open url: URL,
                      options: [UIApplication.OpenURLOptionsKey: Any])
       -> Bool {
+          ApplicationDelegate.shared.application(
+            application,
+              open: url,
+              sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+              annotation: options[UIApplication.OpenURLOptionsKey.annotation]
+          )
+
       return GIDSignIn.sharedInstance.handle(url)
     }
-
-
 }
 
--- DuelDice/DuelDice/Controller/SignViewController.swift
+++ DuelDice/DuelDice/Controller/SignViewController.swift
@@ -9,56 +9,114 @@ import UIKit
 import Firebase
 import FirebaseAuth
 import GoogleSignIn
+import FBSDKLoginKit
 
 class SignViewController: UIViewController {
-    let data: Int = 42
-    @IBOutlet var testLabel: UILabel!
+
+    
     static let showClientSegueIdentifier = "ShowClientDetailSegue"
     
-    @IBOutlet weak var signInButton: GIDSignInButton!
-   
+    @IBOutlet var googleSignInButton: GIDSignInButton!
+    @IBOutlet var emailSignInButton: UIButton!
+    @IBOutlet var signInStackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let facebookSignInButton = FBLoginButton()
+        facebookSignInButton.delegate = self
+        facebookSignInButton.center = signInStackView.center
+        signInStackView.addSubview(facebookSignInButton)
         
-       
     }
     
     override func viewWillAppear(_ animated: Bool) {
-//        if(GIDSignIn.sharedInstance.hasPreviousSignIn()) {
-//            GIDSignIn.sharedInstance.restorePreviousSignIn()
-//            performSegue(withIdentifier: Self.showClientSegueIdentifier, sender: self)
-//          } else {
-//            print("need to login")
-//         }
-        
-        if Auth.auth().currentUser != nil {
-            print("User is signed in.")
-            performSegue(withIdentifier: Self.showClientSegueIdentifier, sender: self)
-        } else {
-            print("No user is signed in.")
-        }
+//        hasPreviousSignIn()
+        emailSignInButton.setTitle("Email Sign-In", for: .normal)
+        emailSignInButton.setTitleColor(UIColor.black, for: .normal)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == Self.showClientSegueIdentifier,
            let destination = segue.destination as? ClientViewController {
-            destination.configure(with: data) {
+            destination.configure(with: 42) {
                 print("\n\nview is changed!\n\n")
             }
         }
     }
 
-    @IBAction func signInButtonTabbed(_ sender: Any) {
+    private func hasPreviousSignIn() {
+        if let user = Auth.auth().currentUser {
+            print("User is signed in. [\(user.uid)]")
+            performSegue(withIdentifier: Self.showClientSegueIdentifier, sender: self)
+        } else {
+            print("No user is signed in.")
+        }
+    }
+    private func firebaseSignIn(with credential: AuthCredential, _ sender: Any) {
+        Auth.auth().signIn(with: credential) { (authResult, error) in
+            if let error = error {
+                print("Firebase(with Google) sign in error: \(error)")
+                return
+            } else {
+                self.performSegue(withIdentifier: Self.showClientSegueIdentifier, sender: sender)
+                print("User is signed with Firebase&Google")
+            }
+        }
+    }
+    private func firebaseCreateUser(withEmail email: String, password: String) {
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                let Autherror = error as NSError
+                switch AuthErrorCode(rawValue: Autherror.code) {
+                case .operationNotAllowed:
+                    // Error: The given sign-in provider is disabled for this Firebase project. Enable it in the Firebase console, under the sign-in method tab of the Auth section.
+                    print("Error: operationNotAllowed")
+                case .emailAlreadyInUse:
+                    // Error: The email address is already in use by another account.
+                    print("Error: emailAlreadyInUse")
+                case .invalidEmail:
+                    // Error: The email address is badly formatted.
+                    print("Error: invalidEmail")
+                case .weakPassword:
+                    // Error: The password must be 6 characters long or more.
+                    print("Error: weakPassword")
+                default:
+                    print("Error: \(error.localizedDescription)")
+                }
+            } else {
+                let newUserInfo = Auth.auth().currentUser
+                //                let email = newUserInfo?.email
+                print("User signs up successfully \(newUserInfo!.uid)")
+            }
+        }
+    }
+}
+extension SignViewController: LoginButtonDelegate {
+    func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?) {
+        if let error = error {
+            print(error.localizedDescription)
+            return
+        }
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
+        print("logout")
+    }
+}
+
+// Button Actions
+// Email, Google, Phone, Facebook, Github
+extension SignViewController {
+    
+
+    @IBAction func googleSignInButtonTabbed(_ sender: Any) {
         guard let clientID = FirebaseApp.app()?.options.clientID else { return }
 
-        // Create Google Sign In configuration object.
         let config = GIDConfiguration(clientID: clientID)
 
-        // Start the sign in flow!
         GIDSignIn.sharedInstance.signIn(with: config, presenting: self) { [unowned self] user, error in
-
             if let error = error {
-                print("GIDSignIn error: \(error)")
+                print("Google Sign-In error: \(error)")
                 return
             }
 
@@ -71,34 +129,61 @@ class SignViewController: UIViewController {
 
             let credential = GoogleAuthProvider.credential(withIDToken: idToken,
                                                          accessToken: authentication.accessToken)
-            
-            Auth.auth().signIn(with: credential) { (authResult, error) in
-                if let error = error {
-                    print("Firebase sign in error: \(error)")
-                    return
-                } else {
-                    performSegue(withIdentifier: Self.showClientSegueIdentifier, sender: sender)
-                    print("User is signed with Firebase&Google")
+            firebaseSignIn(with: credential, sender)
+        }
+    }
+    @IBAction func emailSignInButtonTabbed(_ sender: Any) {
+        let email = "wkdlfflxh@naver.com"
+        let password = "asdf1234"
+
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+            if let error = error {
+                let Autherror = error as NSError
+                
+                switch AuthErrorCode(rawValue: Autherror.code) {
+                case .operationNotAllowed:
+                    // Error: Indicates that email and password accounts are not enabled. Enable them in the Auth section of the Firebase console.
+                    print("Error: operationNotAllowd")
+                case .userDisabled:
+                    // Error: The user account has been disabled by an administrator.
+                    print("Error: userDisabled")
+                case .wrongPassword:
+                    // Error: The password is invalid or the user does not have a password.
+                    print("Error: wrong password")
+                case .invalidEmail:
+                    // Error: Indicates the email address is malformed.
+                    print("Error: invalid Email")
+                case .userNotFound:
+                    // Error: User Not Found.
+                    self?.firebaseCreateUser(withEmail: email, password: password)
+                    print("Error: user Not Found & create User")
+                default:
+                    // Error: Default Error.
+                    print("Error: \(error.localizedDescription), \(Autherror.code)")
                 }
+            } else {
+                guard let strongSelf = self else { return }
+                print("User is signed with Firebase&Email \(strongSelf)")
             }
         }
     }
-    
-    
     
 }
 
-extension SignViewController {
-    func firebaseAnonymousSignIn() {
-        Auth.auth().signInAnonymously { authResult, error in
-            guard let user = authResult?.user else {
-                return
-            }
-            let isAnonymous = user.isAnonymous
-            let uid = user.uid
-            
-            print(uid)
-            print(isAnonymous)
-        }
-    }
-}
+
+// Anonymous Signin
+//
+//extension SignViewController {
+//    func firebaseAnonymousSignIn() {
+//        Auth.auth().signInAnonymously { authResult, error in
+//            guard let user = authResult?.user else {
+//                return
+//            }
+//            let isAnonymous = user.isAnonymous
+//            let uid = user.uid
+//
+//            print(uid)
+//            print(isAnonymous)
+//        }
+//    }
+//}
